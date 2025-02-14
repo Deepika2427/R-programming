31f33d3aaaa99edb2944ca299987dbdb69b71b21
Deepika2427 <148516064+Deepika2427@users.noreply.github.com>
2025-02-14 12:34:10 +0530

    Add files via upload

---
+++ Extract first 10 English letters.R
@@ -0,0 +1,13 @@
+# First 10 lowercase letters
+lowercase_letters <- letters[1:10]
+
+# Last 10 uppercase letters
+uppercase_letters <- toupper(letters[17:26])
+
+# Letters between 22nd to 24th in uppercase
+letters_22_to_24 <- toupper(letters[22:24])
+
+# Print results
+cat("First 10 lowercase letters: ", lowercase_letters, "\n")
+cat("Last 10 uppercase letters: ", uppercase_letters, "\n")
+cat("Letters from 22nd to 24th in uppercase: ", letters_22_to_24, "\n")
