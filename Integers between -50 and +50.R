aec8a0a0145cfcd668173a5aa38bc9638981c3e7
Deepika2427 <148516064+Deepika2427@users.noreply.github.com>
2025-02-14 12:19:05 +0530

    Add files via upload

---
+++ Integers between -50 and +50.R
@@ -0,0 +1,5 @@
+# Create a vector with 10 random integer values between -50 and +50
+random_integers <- sample(-50:50, 10, replace = TRUE)
+
+# Print the vector
+cat("Random integers between -50 and +50: \n", random_integers, "\n")
